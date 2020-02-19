42db9695b4e9cb7e16595c2a2f7ca388f3b81442
whoyoux <48690153+whoyoux@users.noreply.github.com>
2020-02-19 18:27:09 +0100

    Add basic file

---
+++ precache-manifest.16ffc7d27f754917cd5c272761ee1508.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8392295bfd6b8311ed520eab3ffc3d82",
+    "url": "/mc-web/index.html"
+  },
+  {
+    "revision": "83582545e3d38e9f435f",
+    "url": "/mc-web/static/css/main.3bee81ef.chunk.css"
+  },
+  {
+    "revision": "1e30529470a14bfa02fb",
+    "url": "/mc-web/static/js/2.7bfc1c1d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mc-web/static/js/2.7bfc1c1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83582545e3d38e9f435f",
+    "url": "/mc-web/static/js/main.37cca3e6.chunk.js"
+  },
+  {
+    "revision": "39cc79ebd9fdcc7878bc",
+    "url": "/mc-web/static/js/runtime-main.c28adf30.js"
+  },
+  {
+    "revision": "d33e145e9385530619ef7f4213684daa",
+    "url": "/mc-web/static/media/img1.d33e145e.jpg"
+  },
+  {
+    "revision": "ad228c949a31e4df96c9f125ee6e392c",
+    "url": "/mc-web/static/media/img2.ad228c94.jpg"
+  },
+  {
+    "revision": "e1db1483d6ead0ef9dbb42b6edc7bcae",
+    "url": "/mc-web/static/media/img3.e1db1483.jpg"
+  }
+]);
